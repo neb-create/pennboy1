d9ecc13f4242ec0aa0e60027ac5b521f4e5c0774
Faye-Yu <fayeyu0103@gmail.com>
2025-11-01 17:10:28 -0400

    vfx

--- Assets/Note_VFXRingAnimation.cs
+++ Assets/Note_VFXRingAnimation.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class Note_VFXRingAnimation : MonoBehaviour
 {
     [SerializeField] Gradient colorCycle;
+    private SpriteRenderer sr;
     float baseRate;
     float timer;
     public float bpm = 120f;
+    private Vector3 originalScale;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -18,24 +21,22 @@ public class Note_VFXRingAnimation : MonoBehaviour
     {
         float beatInterval = 60f / bpm;
         timer += Time.deltaTime;
-        if (timer >= beatInterval)
-        {
-            timer -= beatInterval;
-            PulseScale();
-        }
+
+        transform.localScale = originalScale * (4f * Mathf.Sin(beatInterval * timer / (2*Mathf.PI)) + 1f);
+
+        timer %= beatInterval;
 
         float t_color = timer / beatInterval;
-
-
+        sr.color = colorCycle.Evaluate(t_color);
     }
     void PulseScale()
     {
-        transform.localScale = Vector3.one * 1.3f;
+        transform.localScale = originalScale * 1.3f;
         Invoke(nameof(ResetScale), 0.1f);
     }
 
     void ResetScale()
     {
-        transform.localScale = Vector3.one;
+        transform.localScale = originalScale;
     }
 }
